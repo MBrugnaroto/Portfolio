6ab2e2cbc4518ceb61b6cff8224853626d9e9515
MBrugnaroto <brugnaroto@gmail.com>
2020-04-24 06:58:04 -0300

    FEAT. Padrão de projeto completo. Configuração de otimização de desenvolvimento completo. Pronto para desenvolver.

--- Project/webpack.config.js
+++ Project/webpack.config.js
@@ -3,19 +3,29 @@
 //utilizamso exports para que essa função entry possa ser utilizada em outro arquivo
 const path = require('path')
 const postCSSPlugins = [
+    require('postcss-import'),
+    require('postcss-mixins'),
     require('postcss-simple-vars'),
     require('postcss-nested'),
     require('autoprefixer')
 ]
 
 module.exports = {
-    entry: './app/assets/scripts/App.js', //Arquivo que será processado, assistido e agrupado
+    entry: './app/assets/scripts/App.js', 
     output: {
         filename: 'bundled.js', 
-        path: path.resolve(__dirname, 'app') //Local aonde será gerado o novo arquivo agrupado
+        path: path.resolve(__dirname, 'app')
+    },
+    devServer: {
+        before: function(app, server) {
+            server._watch('./app/**/*.html')
+        },
+        contentBase: path.join(__dirname, 'app'),
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0'
     },
     mode: 'development',
-    watch: true,
     module: {
         rules: [
             {
