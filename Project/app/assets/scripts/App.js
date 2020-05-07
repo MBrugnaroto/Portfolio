da466443f30ea57c8fac1b5ca69356a63465c092
MBrugnaroto <brugnaroto@gmail.com>
2020-05-06 23:03:29 -0300

    FEAT. Implementação de listeners para aplicar eveitos quando é dado scroll na pagina

--- Project/app/assets/scripts/App.js
+++ Project/app/assets/scripts/App.js
@@ -1,7 +1,10 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
-new MobileMenu();
+let mobileMenu = new MobileMenu();
+new RevealOnScroll(document.querySelectorAll(".exp"), 75);
+new RevealOnScroll(document.querySelectorAll(".service__card"), 60);
 
 if (module.hot) {
     module.hot.accept()
